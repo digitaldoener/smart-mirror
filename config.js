12f21e7fe5be3bb65120bfc98b9164fbff6aad8f
DigitalDoener <digitaldoener@gmail.com>
2016-02-20 23:00:06 +0100

    own configuration

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 var config = {
-    // Lenguage for the mirror (currently not implemented)
-    lenguage : "en",
+    // Language for the mirror (currently not implemented)
+    lenguage : "en", // not used yet
     greeting : ["Hi, sexy!"], // An array of greetings to randomly choose from
     // forcast.io
     forcast : {
-        key : "", // Your forcast.io api key
+        key : "02f5947eb30d855153eedefb91ae233a", // Your forcast.io api key
         units : "auto" // See forcast.io documentation if you are getting the wrong units
     },
     // Philips Hue
     hue : {
         ip : "", // The IP address of your hue base
         uername : "", // The username used to control your hue
-        group : "0", // The group you'd like the mirror to control (0 is all hue lights connected to your hub)
+        group : "0" // The group you'd like the mirror to control (0 is all hue lights connected to your hub)
     },
     // Calendar (An array of iCals)
     calendar: {
